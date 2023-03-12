f40d57ca8242013d0ea0efcd5a54e163a4dad1fb
Sudip Mitra <sudipmitraonline@gmail.com>
2023-03-13 01:00:09 +0530

    react router dom added and navigation added

--- ems-frontend/src/App.js
+++ ems-frontend/src/App.js
@@ -2,15 +2,25 @@ import './App.css';
 import EmployeeListComponents from './components/EmployeeListComponents';
 import FooterComponent from './components/FooterComponent';
 import HeaderComponent from './components/HeaderComponent';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HeaderComponent/>
-      <div className="container">
-        <EmployeeListComponents/>
-      </div>
-      <FooterComponent/>
+      <Router>
+        <div className='container'>
+          <HeaderComponent/>
+            <div className="container">
+              <Routes>
+                <Route path='/' element={<EmployeeListComponents/>}></Route>
+                <Route path='/employees' element={<EmployeeListComponents/>}></Route>
+
+                
+              </Routes>
+            </div>
+          <FooterComponent/>
+        </div>
+      </Router>
     </div>
   );
 }
